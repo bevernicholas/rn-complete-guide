a07244ce53f45e57fd95a17f6ee51b577ec86163
Nick Bever <nicholasbever@Nicholass-MacBook-Air-2.local>
2020-12-19 17:03:25 -0800

    input functionality. Incorporated react hooks

--- App.tsx
+++ App.tsx
@@ -1,14 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [goals, setGoals] = useState<any>([]);
+
+  const goalEnteredHandler = (enteredText: any) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    setGoals((currentGoals: string[]) => [...currentGoals, enteredGoal])
+  }
+
   return (
-    <View style={{padding: 50}}>
-      <View>
-        <TextInput placeholder="Input" style={{borderColor: 'black', borderWidth: 1, padding: 20}}/>
-        <Button title="ADD" />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Input"
+          style={styles.input}
+          onChangeText={goalEnteredHandler}
+          value={enteredGoal}
+        />
+        <Button title="ADD" onPress={addGoalHandler}/>
       </View>
+      <View></View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  input: {
+    width: 200,
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 20,
+  },
+});
